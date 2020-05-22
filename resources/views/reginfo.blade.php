9d82e67cf5800b24f874d4421204ecc803c6e6a1
Alessandro Contu <alecontu1081766@users.github.com>
2020-05-22 18:56:16 +0200

    Aggiunte modalità di registrazione

--- resources/views/reginfo.blade.php
+++ resources/views/reginfo.blade.php
@@ -1,9 +1,22 @@
 @extends ('layouts.public')
-@section ('title', 'Modalità di registrazione')
+@section ('title', 'Modalità dacquisto')
 
 @section('content')
-<div>
-    <h1>Come registrarsi?</h1>
-    <p>Prova testo</p>
+<div class="whoWrapper">
+    <h1 style="text-align: center">Come registrarsi?</h1>
+        <h2>La registrazione può avvenire tramite una delle seguenti modalità:</h2>
+        <ul>
+            <li><h3>Cliccando direttamente sul tasto REGISTRATI posizionato in alto a destra</h3></li>
+            <hr>
+            <img class="card_image" style="border: solid black 1px" src="{{asset('images/regist1.jpg')}}" alt="image">
+            <li><h3>Cliccando sul pulsante registrati nella finestra di accesso</h3></li>
+            <hr>
+            <img class="card_image" style="border: solid black 1px" src="{{asset('images/fondatore.jpg')}}" alt="image">
+            <li><h3>Oppure puoi cliccare direttamente <a style="color: red" href="">QUI</a> </h3></li>
+            <hr>
+        </ul>
+    
+    
 </div>
 @endsection
+        
