4a8c777b02b20268906d0d00003f1307ca0e5583
loren <loren@192.168.30.20>
2020-05-22 16:30:30 +0200

    Aggiunto Public Controller

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,17 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Catalog;
 
 class PublicController extends Controller
 {
+       protected $catalog;
+    
     /*Costruttore*/
     public function __construct() {
-        
+        $this->catalog=new Catalog();
     }
     
     public function showHome() {
         return view('home');
     }
     
+    public function showCatalog1() {
+        $topCategories = $this->catalog->getTopCategories();
+
+        $products = $this->catalog->getProducts($topCategories->map->only(['codCat']), 4, 'asc');
+
+        return view('catalog')
+                        ->with('topCategories', $topCategories)
+                        ->with('products', $products);
+    }
+    
+    public function showCatalog2($topCodCat) {
+
+        //Categorie Top
+        $topCategories = $this->catalog->getTopCategories();
+
+        //Categoria Top selezionata
+        $selectedTopCategories = $topCategories->where('codCat', $topCodCat);
+
+        // Sottocategorie
+        $subCategories = $this->catalog->getSubCategories([$topCodCat]);
+                        
+        //Prodotti in sconto della categoria Top selezionata, ordinati per sconto decrescente 
+        $products = $this->catalogModel->getProducts([$topCodCat], 4, 'asc');
+
+        return view('catalog')
+                        ->with('topCategories', $topCategories)
+                        ->with('selectedTopCategories', $selectedTopCategories)
+                        ->with('subCategories', $subCategories)
+                        ->with('products', $products);
+    }
 }
--- app/Models/Catalog.php
+++ app/Models/Catalog.php
@@ -9,18 +9,18 @@ use App\Models\Resources\Product;
 class Catalog extends Model
 {
     /*Metodo per prelevare tutte le macrocategorie*/
-     public function getTopCategory() {
+     public function getTopCategories() {
         return Category::where('codPar', 0)->get();
     }
     
     /*Metodo per prelevare tutte le sottocategorie
      */
-    public function getSubCategory($codPar) {
+    public function getSubCategories($codPar) {
         return Category::whereIn('codPar', $codPar)->get();
     }
     
     /*Metodo per estrarre i prodotti della macrocategoria o sottocategoria*/
-    public function getProduct($codCat, $paged, $order=null) {
+    public function getProducts($codCat, $paged, $order=null) {
         /*Metodo che consente di estrarre tutti i prodotti che hanno una categoria contenuta in $codCat oppure
          * che hanno una categoria che ha come padre quella che specifichiamo come $codCat
          */
