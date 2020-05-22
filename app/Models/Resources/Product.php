ab8476c663d15f6927997e87f2cd9bd2efa94723
loren <loren@192.168.30.20>
2020-05-22 15:21:45 +0200

    Aggiunto Model Prodotto

---
+++ app/Models/Resources/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table= "prodotto";
+    protected $primaryKey = 'codProd';
+
+    // prodId non modificabile da un HTTP Request (Mass Assignment)
+    protected $guarded = ['codProd'];
+    public $timestamps = false;
+    
+    /*Metodo che calcola il prezzo del prodotto tenendo conto di un eventuale sconto*/
+    public function getPrice() {
+        $prezzo= $this->prezzo;
+        if ($this->sconto==true) {
+            $sconto = ($prezzo * $this->percSconto) / 100;
+            $prezzo = round($prezzo - $sconto, 2);
+        }
+        return $prezzo;
+    }
+    
+    // Realazione One-To-One con Categoria
+    public function prodCat() {
+        return $this->hasOne(Category::class, 'codCat', 'codCat');
+    }
+}
