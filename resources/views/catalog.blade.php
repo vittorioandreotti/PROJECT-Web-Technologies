ecf886de1dbef10e9a02e170d1f59a51cc9c0d52
loren <loren@192.168.30.20>
2020-05-22 21:45:57 +0200

    Corretto catalog.php

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -2,31 +2,28 @@
 @section('title', 'Catalogo')
 
 @section('content')
-<div id="catalogoContainer">
-    @isset($products)
-    <div class="catalogoProdotti">
-    <h2>RAM</h2>
-    <span>Benvenuto nella sezione *Ram* di iPrice.<br> Scegli tra tantissimi prodotti in offerta e con consegna rapida.<br> Scopri la nostra ampia proposta, consulta i prezzi e acquista comodamente online.</span>
-    <div>
-        <input type="text" placeholder="Cerca">
-    </div>
-        @foreach ($products as $product)
-        <div id="containerProdotti">
-                        <div class="containerProdotto">
-                            <img src="{{ asset('images/products/' . $product->image) }}">
-                             <div class="info">
-                                <h2 class="title">Prodotto: {{ $product->nome }}</h2>
-                                <p class="meta">Prezzo: {{ $product->prezzo }}</p>
-                             </div>
-                        </div>
-        @endforeach
-        </div>
-    </div>
-    @endisset()
-
-    <div id="sideMenuCatalogo">
+<div id="containerCatalogo">
+    <div id="menuCatalogo">
         @include('layouts/menuCatalog')
     </div>
-    
+    <div id='contentCatalogo'>
+        <h2>RAM</h2>
+        <p id='totaleProdotti'>Totale prodotti: {{count($products)}}</p>
+        <span>Benvenuto nella sezione *Ram* di iPrice.<br> Scegli tra tantissimi prodotti in offerta e con consegna rapida.<br> Scopri la nostra ampia proposta, consulta i prezzi e acquista comodamente online.</span>
+        <div id='cerca'>
+            <input type="text" placeholder="Cerca">
+        </div>
+        <div id='containerProdotti'>
+            @foreach($products as $product)
+            <div id="containerProdotto">
+                <img src="{{ asset('images/products/' . $product->image) }}">
+                <div class="info">
+                    <h2 class="title">Prodotto: {{ $product->nome }}</h2>
+                    <p class="price"> Prezzo: {{ $product->prezzo }} € </p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @endsection
