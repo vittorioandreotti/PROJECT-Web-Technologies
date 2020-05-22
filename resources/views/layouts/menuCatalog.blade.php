cadd2274af850082c4de628ed9e899f4ce348c96
Lorenzo Iacopini <Lorenzo Iacopini@LAPTOP-JSADJJPV>
2020-05-22 16:55:39 +0200

    menucatalog

---
+++ resources/views/layouts/menuCatalog.blade.php
@@ -0,0 +1,21 @@
+ <ul id="menuCatalogo">
+          <li ><a href="">Componenenti PC</a>
+           <ul>
+                      <li> <a href="">RAM</a></li>
+                      <li> <a href="">Schede Video</a></li>
+                      <li> <a href="">Schede Madri</a></li>
+                      <li> <a href="">Processori</a></li>
+                  </ul></li>
+          
+          <li ><a href="">Audio</a> <ul>
+                      <li> <a href="">AirPods</a></li>
+                      <li> <a href="">Cuffie</a></li>
+                      <li> <a href="">Registratori</a></li>
+                      <li> <a href="">Console DJ</a></li>
+                  </ul></li>
+          <li ><a href="">Fotografia</a> <ul>
+                      <li> <a href="">Reflex</a></li>
+                      <li> <a href="">Videocamere</a></li>
+                  </ul></li>
+          
+        </ul>
