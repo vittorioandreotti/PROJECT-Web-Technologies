a913911ad749a29acd82719943f469595bd6c837
Vittorio Andreotti <andreottivittorio@gmail.com>
2020-05-23 18:42:35 +0200

    Registrazione

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                 {{ Form::submit('Login', ['class' => 'form-btn1']) }}
             
             
-                <p> Se non hai già un account <a  href="{{route('register')}}">registrati</a></p>   
+                <p> Se non hai già un account <a  href="{{route('register')}}">Registrati</a></p>   
             {{ Form::close() }}
     </div>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,117 +3,99 @@
 
 @section('content')
 
-<div>
-    <h3>Registrazione</h3>
-    
-    <div>
-        {{Form::open(array('route' => 'register'))}}
-        
-        <div>
-            {{Form::label('name', 'Nome')}}
-            {{Form::text('name', ''), ['id' => 'name']}}
-            @if ($errors->first('name'))
-            <ul class="errors">
-                @foreach ($errors->get('name') as $message)
-                <li>{{ $message }}</li>
-                @endforeach
-            </ul>
-            @endif
-            
-        </div>
-            <div  class="wrap-input">
-                {{ Form::label('surname', 'Cognome', ['class' => 'label-input']) }}
-                {{ Form::text('surname', '', ['class' => 'input', 'id' => 'surname']) }}
-                @if ($errors->first('surname'))
-                <ul class="errors">
-                    @foreach ($errors->get('surname') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
-            
-             <div  class="wrap-input">
-                {{ Form::label('username', 'Nome Utente', ['class' => 'label-input']) }}
-                {{ Form::text('username', '', ['class' => 'input','id' => 'username']) }}
-                @if ($errors->first('username'))
-                <ul class="errors">
-                    @foreach ($errors->get('username') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
-            
-             <div  class="wrap-input">
-                {{ Form::label('email', 'Email', ['class' => 'label-input']) }}
-                {{ Form::text('email', '', ['class' => 'input','id' => 'email']) }}
-                @if ($errors->first('email'))
-                <ul class="errors">
-                    @foreach ($errors->get('email') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
-            
-             <div  class="wrap-input">
-                {{ Form::label('password', 'Password', ['class' => 'label-input']) }}
-                {{ Form::password('password', ['class' => 'input', 'id' => 'password']) }}
-                @if ($errors->first('password'))
-                <ul class="errors">
-                    @foreach ($errors->get('password') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
+<div class="container spazio clearfix">
+    <div class="box">
+        <h3>Registrazione</h3>
 
-            <div  class="wrap-input">
-                {{ Form::label('password-confirm', 'Conferma password', ['class' => 'label-input']) }}
-                {{ Form::password('password_confirmation', ['class' => 'input', 'id' => 'password-confirm']) }}
-            </div>
+        {{Form::open(array('route' => 'register'))}}
+
+        {{Form::label('name', 'Nome')}}
+        {{Form::text('name', ''), ['id' => 'name']}}
+        @if ($errors->first('name'))
+        <ul class="errors">
+            @foreach ($errors->get('name') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::label('surname', 'Cognome') }}
+        {{ Form::text('surname', '', ['class' => 'input', 'id' => 'surname']) }}
+        @if ($errors->first('surname'))
+        <ul class="errors">
+            @foreach ($errors->get('surname') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::label('username', 'Username') }}
+        {{ Form::text('username', '', ['id' => 'username']) }}
+        @if ($errors->first('username'))
+        <ul class="errors">
+            @foreach ($errors->get('username') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::label('email', 'Email') }}
+        {{ Form::text('email', '', ['id' => 'email']) }}
+        @if ($errors->first('email'))
+        <ul class="errors">
+            @foreach ($errors->get('email') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::label('password', 'Password') }}
+        {{ Form::password('password', ['id' => 'password']) }}
+        @if ($errors->first('password'))
+        <ul class="errors">
+            @foreach ($errors->get('password') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::label('password-confirm', 'Conferma password', ['class' => 'label-input']) }}
+        {{ Form::password('password_confirmation', ['id' => 'password-confirm']) }}
+
+        {{ Form::label('job', 'Occupazione') }}
+        {{ Form::text('job', '',  ['id' => 'job']) }}
+        @if ($errors->first('job'))
+        <ul class="errors">
+            @foreach ($errors->get('job') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
         
-            <div  class="wrap-input">
-                {{ Form::label('job', 'Occupazione', ['class' => 'label-input']) }}
-                {{ Form::password('job', ['class' => 'input', 'id' => 'job']) }}
-                @if ($errors->first('job'))
-                <ul class="errors">
-                    @foreach ($errors->get('job') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
-        
-            <div  class="wrap-input">
-                {{ Form::label('dateOfBirth', 'Data di nascita', ['class' => 'label-input']) }}
-                {{ Form::password('dateofBirth', ['class' => 'input', 'id' => 'dateOfBith']) }}
-                @if ($errors->first('job'))
-                <ul class="errors">
-                    @foreach ($errors->get('dateOfBirth') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
-        
-            <div  class="wrap-input">
-                {{ Form::label('homeTown', 'Luogo di residenza', ['class' => 'label-input']) }}
-                {{ Form::password('homeTown', ['class' => 'input', 'id' => 'homeTown']) }}
-                @if ($errors->first('job'))
-                <ul class="errors">
-                    @foreach ($errors->get('homeTown') as $message)
-                    <li>{{ $message }}</li>
-                    @endforeach
-                </ul>
-                @endif
-            </div>
-            <div class="container-form-btn">                
-                {{ Form::submit('Registra', ['class' => 'form-btn1']) }}
-            </div>
-            
-            {{ Form::close() }}
-        </div>
+        {{ Form::label('dateOfBirth', 'Data di nascita') }}
+        {{ Form::text('dateofBirth', '', ['id' => 'dateOfBith']) }}
+        @if ($errors->first('dateOfBirth'))
+        <ul class="errors">
+            @foreach ($errors->get('dateOfBirth') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::label('homeTown', 'Luogo di residenza') }}
+        {{ Form::text('homeTown', '', ['id' => 'homeTown']) }}
+        @if ($errors->first('homeTown'))
+        <ul class="errors">
+            @foreach ($errors->get('homeTown') as $message)
+            <li>{{ $message }}</li>
+            @endforeach
+        </ul>
+        @endif
+
+        {{ Form::submit('Registra') }}
+
+        <p>Se hai già un account <a href="{{route("login")}}">Accedi</a></p>
+        {{ Form::close() }}
     </div>
+</div>
 @endsection
