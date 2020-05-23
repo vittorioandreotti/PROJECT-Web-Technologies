4c38e79603e4bc544359718aea7dfd2486782b37
loren <loren@192.168.30.20>
2020-05-23 15:10:06 +0200

    Modifica MenuCatalog

--- resources/views/layouts/menuCatalog.blade.php
+++ resources/views/layouts/menuCatalog.blade.php
@@ -1,6 +1,12 @@
 <h3>CATEGORIE</h3> 
 <ul>
      @foreach($topCategories as $category)
-     <li><a href="">{{$category->name}}</a></li>
+     <li><a href="">{{$category->name}}</a>
+         @isset($subCategories)
+         <ul>
+             <a href="">{{$subCategories->name}}</a>
+         </ul>
+         @endisset()
+     </li>
      @endforeach
 </ul>
