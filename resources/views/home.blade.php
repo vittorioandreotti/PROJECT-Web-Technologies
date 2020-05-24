b7e2848295f8fd2ffd85a929d0e19d64fdb2befa
loren <loren@192.168.30.20>
2020-05-24 16:55:49 +0200

    Aggiungi mappe d'area nella home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,19 +13,28 @@
           <div class="card">
               <div class="card_copy">
                   <h3>COMPONENTI PC</h3>
-                  <img class="card_image" src="{{asset('images/componenti.jpg')}}" alt="image">
+                  <img class="card_image" src="{{asset('images/componenti.jpg')}}" alt="Immagine componenti pc" usemap="#componentiPC">
+                  <map name="#componentiPC">
+                      <area shape="rect" coords="0 260 400 0" href="{{route('catalog2',[1])}}">
+                  </map>
               </div>
           </div>
           <div class="card">
               <div class="card_copy">
                   <h3>AUDIO</h3>
-                  <img class="card_image" src="{{asset('images/audio.jpg')}}" alt="image">
+                  <img class="card_image" src="{{asset('images/audio.jpg')}}" alt="image" usemap="#audio">
+                  <map name="#audio">
+                    <area shape="rect" coords="0 260 400 0" href="{{route('catalog2',[2])}}">
+                  </map>
               </div>
           </div>
           <div class="card">
               <div class="card_copy">
                   <h3>FOTOGRAFIA</h3>
-                  <img class="card_image" src="{{asset('images/foto.jpg')}}" alt="image">
+                  <img class="card_image" src="{{asset('images/foto.jpg')}}" alt="image" usemap="#fotografia">
+                  <map name="#fotografia">
+                    <area shape="rect" coords="0 260 400 0" href="{{route('catalog2',[3])}}">
+                  </map>
                 </div>
             </div>
     </section>
