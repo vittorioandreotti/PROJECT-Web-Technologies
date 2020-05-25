7df3fd6b7efc6bc8c0fa0650759cc0084e27eee8
loren <loren@192.168.30.20>
2020-05-25 08:53:39 +0200

    Cambiati nomi attributi migration utente

--- database/migrations/2020_05_23_113503_create_utente.php
+++ database/migrations/2020_05_23_113503_create_utente.php
@@ -15,15 +15,15 @@ class CreateUtente extends Migration
     {
         Schema::create('utente', function (Blueprint $table) {
             $table->bigIncrements('codUser')->index();
-            $table->string('name');
-            $table->string('surname');
+            $table->string('nome');
+            $table->string('cognome');
             $table->string('username')->unique();
             $table->string('password');
             $table->string('email');
-            $table->string('role', 10)->default('user');
-            $table->string('homeTown');
-            $table->date('dateOfBirth');
-            $table->string('job');
+            $table->string('ruolo', 10)->default('user');
+            $table->string('residenza');
+            $table->date('dataNascita');
+            $table->string('occupazione');
         });
     }
 
