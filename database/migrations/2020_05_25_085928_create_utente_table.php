836d1979ab593117ba1742e6b9a8343b7abd1bb3
Vittorio Andreotti <andreottivittorio@gmail.com>
2020-05-25 11:37:15 +0200

    Esportata migration utente in altro file

---
+++ database/migrations/2020_05_25_085928_create_utente_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtenteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utente', function (Blueprint $table) {
+            $table->bigIncrements('codUser')->unsigned()->index();
+            $table->string('nome');
+            $table->string('cognome');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('email');
+            $table->string('ruolo',10)->default('user');
+            $table->string('residenza');
+            $table->string('dataNascita');
+            $table->string('occupazione');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utente');
+    }
+}
