365e0b847bcff91444f0c5d6e7c0432bea91d52d
Alessandro Contu <alecontu1081766@users.github.com>
2020-05-26 15:24:42 +0200

    Fix nomi

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'ADMIN')
 
 @section('content')
-<div class="static">
+<div>
     <h3>Area Amministratore</h3>
-    <p>Benvenuto {{ Auth::user()->name }} {{ Auth::user()->surname }}</p>
+    <p>Benvenuto {{ Auth::user()->nome }} {{ Auth::user()->cognome }}</p>
     <p>Seleziona nella barra la funzione che vuoi utilizzare</p>
 </div>
 @endsection
