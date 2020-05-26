23003f3fbcc9862e8640c0ea66841dad7c2268f6
Alessandro Contu <alecontu1081766@users.github.com>
2020-05-26 15:36:44 +0200

    Fix nomi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ namespace App\Http\Controllers;
 class UserController extends Controller
 {
     public function __construct() {
-        $this->middleware('auth');
+        $this->middleware('can:isUser');
     }
 
     public function index() {
