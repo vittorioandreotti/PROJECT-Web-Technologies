a79e4c781287bc9380e176df75aa407cc114e418
loren <loren@192.168.30.20>
2020-05-29 11:04:51 +0200

    Correzioni CSS-2

--- resources/views/layouts/menuUser.blade.php
+++ resources/views/layouts/menuUser.blade.php
@@ -8,8 +8,8 @@
     @endauth
     <ul class="headerMenu">
               <li class="headerMenuItem"><a href="{{ route('home') }}">Home</a></li>
-              <li class="headerMenuItemUser"><a href="{{ route('editProfile') }}" title="Modifica il tuo profilo">Modifica Profilo</a></li>
-              <li class="headerMenuItemUser"><a href="{{ route('editPassword') }}" title="Modifica la tua password">Modifica Password</a></li>
+              <li class="headerMenuItemAuth"><a href="{{ route('editProfile') }}" title="Modifica il tuo profilo">Modifica Profilo</a></li>
+              <li class="headerMenuItemAuth"><a href="{{ route('editPassword') }}" title="Modifica la tua password">Modifica Password</a></li>
                @auth
                 <li class="headerMenuItem" id="logout"><a href="" class="" title="Esci dal sito" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
